fa9fc9d0f3851f20b7dbfa852c4aade142bf5671
bastien.grisvard <bastien.grisvard@raccourci.fr>
2025-12-18 10:10:11 +0100

    remove --platform from plugin dockerfile

--- shared/config/config.go
+++ shared/config/config.go
@@ -1,12 +1,11 @@
 package config
 
 type Config struct {
-	BrokerUrl        string `env:"BROKER_URL, required"`
-	SqliteDbPath     string `env:"SQLITE_DB_PATH,required"`
-	ApiPort          int    `env:"API_PORT,default=8080"`
-	PluginFolderPath string `env:"PLUGIN_FOLDER_PATH,required"`
-	SessionSecret    string `env:"SESSION_SECRET"`
-	AppEnv           AppEnv `env:"APP_ENV,default=dev"`
+	BrokerUrl     string `env:"BROKER_URL, required"`
+	SqliteDbPath  string `env:"SQLITE_DB_PATH,required"`
+	ApiPort       int    `env:"API_PORT,default=8080"`
+	SessionSecret string `env:"SESSION_SECRET"`
+	AppEnv        AppEnv `env:"APP_ENV,default=dev"`
 }
 
 type AppEnv string
